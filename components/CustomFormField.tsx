cf77ad16979cc0d8c81fe9dc66c42c5dfb39559c
Olivia <56949536+oliviakotova@users.noreply.github.com>
2024-10-16 14:51:03 +1000

    06

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconsAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+}
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  switch (props.fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400"></div>
+      );
+
+    default:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
