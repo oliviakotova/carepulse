5b9f0f943608cd4bea4f2afacb9a2bb6e04746ad
Olivia <56949536+oliviakotova@users.noreply.github.com>
2024-10-18 15:52:16 +1000

    create new patient

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -63,7 +63,7 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
       return (
         <FormControl>
           <PhoneInput
-            defaultContry="au"
+            defaultcontry="au"
             placeholder={placeholder}
             international
             withCountryCallingCode
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,14 +3,17 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+
+import { Dispatch, SetStateAction, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
-import { useState } from "react";
+
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
 import { register } from "module";
+import { createUser } from "@/lib/actions/patient.action";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -23,8 +26,9 @@ export enum FormFieldType {
 }
 
 const PatientForm = () => {
-  const router = useRouter;
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
     defaultValues: {
@@ -42,12 +46,17 @@ const PatientForm = () => {
     setIsLoading(true);
 
     try {
-      //const userData ={name,email,phone};
-      //const user = await createUser(userData);
-      //if(user) router.push(`/patients/${user.$id}/register`)
+      const userData = { name, email, phone };
+
+      const user = await createUser(userData);
+
+      if (user) {
+        router.push(`/patients/${user.$id}/register`);
+      }
     } catch (error) {
       console.log(error);
     }
+    setIsLoading(false);
   }
 
   return (
